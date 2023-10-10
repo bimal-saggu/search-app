a8e2182c847499b4227e39699c54b265692959d5
Bimal Saggu <bimalsaggu28@gmail..com>
2023-10-10 15:31:05 +0530

    Build a navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/zevi.webp";
+const NavBar = () => {
+  return (
+    <HStack margin={7}>
+      <Image src={logo} width={20} />
+    </HStack>
+  );
+};
+
+export default NavBar;
